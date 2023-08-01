4f6b8a51975505edd8676784e90be458f3de53aa
ShakaPursuit <shakalivngstone@pursuit.org>
2023-08-01 02:48:36 -0400

    passed object props with .map loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import TopBar from "./Components/TopBar";
 import "./App.css";
+import DonationForm from "./Components/DonationForm";
+import { useState } from "react";
+import RecentDonations from "./Components/RecentDonations";
+import Progress from "./Components/Progress";
 
 const targetAmount = 1000;
-const donations = [
+
+const Donation=()=>{
+const [donations,setDonations] =useState([
   {
     amount: 250,
     caption: "You really need this. Really.",
@@ -34,17 +40,57 @@ const donations = [
     id: 5,
     name: "Sam",
   },
-];
+]);
+// const PriceUpdate=()=>{
+//   const [progress,setProgress] =useState([
+//     {
+//       amount: 250,
+//       caption: "You really need this. Really.",
+//       id: 1,
+//       name: "Jo",
+//     },
+//     {
+//       amount: 30,
+//       caption: "Here, take a break from work!",
+//       id: 2,
+//       name: "Rami",
+//     },
+//     {
+//       amount: 20,
+//       caption: "LOL! You are too funny. Happy to do this for you. :)",
+//       id: 3,
+//       name: "Michelle",
+//     },
+//     {
+//       amount: 5,
+//       caption: "Have fun!",
+//       id: 4,
+//       name: "Malinda",
+//     },
+//     {
+//       amount: 30,
+//       caption: "Come visit me in Miami!",
+//       id: 5,
+//       name: "Sam",
+//     },
+//   ]);
+
+
+//   return(<div><Progress progress={progress}/></div>);
+// }
+
+return (<div><RecentDonations key="recent-donations" donations={donations}/></div>);
+}
 
 function App() {
   return (
     <>
       <TopBar />
       <main className="container">
-        <section className="sidebar">{/* Recent Donations */}</section>
+        <section className="sidebar">{<Donation/>}</section>
         <section className="">
-          {/* Progress */}
-          {/* Donation Form */}
+          {<Progress/>}
+          {<DonationForm/>}
         </section>
       </main>
     </>
--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,39 @@
-export default function Progress() {
-  return null;
+
+export default function Progress(props) {
+
+   const progressUpdates=props.progress
+
+  // for (let i=0;i<progressUpdates.length;i++){
+
+  //   return progressUpdates[i].amount+=progressUpdates[i].amount
+
+
+
+  // }
+  return (
+    
+
+
+    <div id="price-update">
+        <h1>Help Us Reach Our Goals</h1>
+
+        <h2>$ 335 Donated out of $1,000</h2>
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+  
+
+
+  );
 }
