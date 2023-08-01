e9e5854996597c2c8cbac78d6666fbe81f2d1d32
Rose <i.a.rose.bk@gmail.com>
2023-08-01 19:11:36 -0400

    updated readme w/ netlify

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,19 @@
-export default function Progress() {
-  return null;
+export default function Progress( targetAmountFromApp) {
+  return (
+    <>  
+    <section className="progress">
+    <h2>
+      Raised <span className="secondary">$0</span> of
+      <span className="secondary">$1000</span>
+    </h2>
+  </section>
+    <section className="progress">
+<h2>
+Raised  <span className= "secondary"> $5  </span> of 
+<span className= "secondary"> {$props.targetAmountFromApp} </span>
+</h2>
+    </section>
+    </>
+  )
+  // null;
 }
