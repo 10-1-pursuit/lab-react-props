b53bd91a07eec3a886135626574ccc80086c956b
Lamb144 <alvinjohnson144@gmail.com>
2023-08-14 00:35:02 -0400

    One of One

--- src/Components/DonationForm.jsx
+++ src/Components/DonationForm.jsx
@@ -1,6 +1,30 @@
 function DonationForm() {
-  return {
-
-  };
+  return (
+    <>
+      <section className="donation">
+        <form>
+          <label htmlFor="name"
+          >Name<input
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Your name..." /></label
+          ><label htmlFor="caption"
+          >Caption<input
+              id="caption"
+              name="caption"
+              type="text"
+              placeholder="Add a brief message..." /></label
+          ><label htmlFor="amount"
+          >Amount<input
+              id="amount"
+              name="amount"
+              type="number"
+              placeholder="0" /></label
+          ><button>Donate!</button>
+        </form>
+      </section>
+    </>
+  )
 }
 export default DonationForm;
--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,6 +1,12 @@
-function Progress() {
+const Progress = ({ amountOfDonations, targetAmount, nextDonationUp }) => {
   return (
-    
+    <>
+      <h2>
+        Raised <span className="secondary">${amountOfDonations}</span> of
+        <span className="secondary"> ${targetAmount}</span>
+        <div className="progress"><h3>You could be donation<span className="secondary"> #{nextDonationUp}!</span></h3></div>
+      </h2>
+    </>
   )
 }
 export default Progress;
