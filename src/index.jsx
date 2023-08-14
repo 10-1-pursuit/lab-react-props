3d71863fc51c9a77e9de0fdd4aea34fcceb0806d
Lamb144 <alvinjohnson144@gmail.com>
2023-08-14 06:59:41 -0400

    Final Commit(React 18 createRoot Method , new dependencies initalized. check index.jsx comments)

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+// added the below to use the new React 18 createRoot method.
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-ReactDOM.render(
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
-);
+)
+
+// Changed the below to the above to use the new React 18 createRoot method.
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
