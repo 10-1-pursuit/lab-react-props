2e1eb7e97e529667135439bf4df34637ed3ce6b5
Kenneth-Parker <kennethkavon@gmail.com>
2023-08-16 04:43:06 -0400

    recent and progress updated w app.jsx

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,14 @@
-export default function Progress() {
-  return null;
+import React from "react";
+
+function Progress({ targetAmount, allDonations }) {
+
+  return (
+    <section className='progress'>
+      <h2>
+        Raised <span className="secondary">${allDonations}</span> of
+        <span className="secondary">${targetAmount}</span>
+      </h2>
+    </section>
+  );
 }
+export default Progress;
