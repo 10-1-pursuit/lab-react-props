364a46df1bd4da9bf241c4756739eb7d6cea7830
christophersanchez <christophersanchez@pursuit.org>
2023-08-19 20:37:36 -0400

    Updated Progress.jsx ✨

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,17 @@
-export default function Progress() {
-  return null;
+import React from 'react';
+
+function Progess () {
+
+  const totalRaised = props.donations.reduce((total, donations) => total + donations.amount, 0);
+
+  return (
+<section className="progress">
+  <h2>
+    Raised <span className="secondary">$(totalRaised)</span> of
+    <span className="secondary">{props.targetAmount}</span>
+  </h2>
+</section>
+  );
 }
+
+export default Progess;
