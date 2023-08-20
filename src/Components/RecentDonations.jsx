988c7308ef7350d4bb3cd4b3bcf09cd04556b325
christophersanchez <christophersanchez@pursuit.org>
2023-08-19 20:46:06 -0400

    Updated RecentDonations.jsx ✨

--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,23 @@
-export default function RecentDonations() {
-  return null;
+import React from 'react';
+
+function RecentDonations(props) {
+  const recentDonations = props.donations.map((donation) => (
+    <li key= {donation.id}>
+      <span>{donation.name} donated ${donation.amount}</span>
+      {donation.caption}
+    </li>
+  ));
+
+  return (
+    <section>
+  <h2>Recent Donations</h2>
+  <ul>
+    <li><span>Jo donated $25</span>You really need this. Really.</li>
+    <li><span>Rami donated $10</span>Here, take a break from work!</li>
+  </ul>
+</section>
+  );
 }
+
+
+export default RecentDonations;
