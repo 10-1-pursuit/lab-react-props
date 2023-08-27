917c1b8c56ec82d1baaf4ca133e051ee2eb87677
Yulonda <yulondawyatt@pursuit.org>
2023-08-26 22:53:22 -0400

    Place html snippets in respective components.  Next task is to make components dynamic

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -2,6 +2,16 @@
 
 export default function Progress() {
   return (
+    <div>
     <p>PROGRESS - Raised _some amount_ from _this amount_</p>
+
+    
+    <section className="progress">
+  <h2>
+    Raised <span className="secondary">$0</span> of
+    <span className="secondary">$1000</span>
+  </h2>
+</section>
+  </div>
   );
 }
--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,7 +1,16 @@
 export default function RecentDonations() {
  return (
  <div>
-  <p>_ Recent Donations _</p>
+
+  <section>
+  <h2>Recent Donations</h2>
+  <ul>
+    <li><span>Jo donated $25</span>You really need this. Really.</li>
+    <li><span>Rami donated $10</span>Here, take a break from work!</li>
+    
+  </ul>
+</section>
+
  </div>
  );
 }
